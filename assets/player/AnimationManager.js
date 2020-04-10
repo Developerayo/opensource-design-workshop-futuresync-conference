c71b485cbe9146684299a0cabd5d8118b4ea6eb5
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-04-10 17:02:20 +0100

    Chore: Animation Improvements

--- assets/player/AnimationManager.js
+++ assets/player/AnimationManager.js
@@ -8,6 +8,16 @@ var AnimationManager = Class.create({
     this.styleSheet = document.styleSheets[document.styleSheets.length - 1];
     this.createdAnimations = new Object();
   },
+  createAnimation: function (a) {
+    this.styleSheet.insertRule(kKeyframesPropertyName + " " + a + " {}", 0);
+    return this.styleSheet.cssRules[0];
+  },
+  deleteAllAnimations: function () {
+    while (this.styleSheet.cssRules.length > 0) {
+      this.styleSheet.deleteRule(0);
+    }
+    this.createdAnimations = new Object();
+  },
   markAnimationsCreated: function (a) {
     this.createdAnimations[a] = true;
   },
